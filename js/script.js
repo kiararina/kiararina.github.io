d91f332b9b16d44beb845c1067b1e79c68150f75
krpm <25734153+kiararina@users.noreply.github.com>
2022-12-16 03:37:25 -0500

    Added form validation.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function displayRate() {
+    if (document.getElementById('hiring').checked) {
+        document.getElementById('hiring-input').style.display = 'block';
+    }
+    else document.getElementById('hiring-input').style.display = 'none';
+
+}
